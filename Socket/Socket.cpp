417ec147b8de3b2c099febf108c8a7f0f688cde8
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-06-24 22:20:15 +0900

    refactor: multiple server socket, modify class struct

--- Socket/Socket.cpp
+++ Socket/Socket.cpp
@@ -1,42 +1,83 @@
 #include "Socket.hpp"
 
 ws::Socket::Socket(const ws::Configure& cls) {
-	std::vector<ws::Server> server = cls.get_server();
+  std::vector<ws::Server> server_config = cls.get_server();
 
-	for (int i = 0; i < server.size(); i++) {
-		ws::Server::listen_type listen = server[i].get_listen();
+  _kq = kqueue();
+  if (_kq == -1)
+    throw; // require custom exception
 
-		for (int j = 0; j < server[i].get_listen().size(); j++){
-			std::pair<int, int> server_fd;
-			if ((server_fd.first = socket(PF_INET, SOCK_STREAM, 0)) == -1)
-				throw; // require custom exception
-			if ((server_fd.second = kqueue()) == -1)
-				throw; // reuqire custom exception
-			_server_fd.push_back(server_fd);
+  for (int i = 0; i < server_config.size(); i++) {
+    ws::Server::listen_type listen = server_config[i].get_listen();
 
-			struct sockaddr_in server_addr;
+    for (int j = 0; j < server_config[i].get_listen().size(); j++){
+      std::pair<int, struct sockaddr_in> server_pair;
+      if ((server_pair.first = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+        throw; // require custom exception
 
-			memset(&server_addr, 0, sizeof(server_addr));
+      memset(&server_pair.second, 0, sizeof(server_pair.second));
+      server_pair.second.sin_family = AF_INET;
+      server_pair.second.sin_addr.s_addr = listen[j].first;
+      server_pair.second.sin_port = listen[j].second;
+      _server.insert(server_pair);
+    }
+  }
 
-			server_addr.sin_family = AF_INET;
-			server_addr.sin_addr.s_addr = listen[j].first;
-			server_addr.sin_port = listen[j].second;
+  for (server_type::iterator it = _server.begin(); it != _server.end(); ++it) {
+    if (bind(it->first, (struct sockaddr*)&it->second, sizeof(it->second)) == -1)
+      throw; // reuqire custom exception
+    if (listen(it->first, 5) == -1)
+      throw; // reuqire custom exception
+    fcntl(it->first, F_SETFL, O_NONBLOCK);
+    add_new_kevent(_change_list, it->first, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+  }
 
-			_server_addr.push_back(server_addr);
-		}
-	}
-
-	for (int i = 0; i < _server_fd.size(); i++) {
-		if (bind(_server_fd[i].first, (struct sockaddr*)&_server_addr[i], sizeof(_server_addr[i])) == -1)
-			throw; // reuqire custom exception
-		if (listen(_server_fd[i].first, 5) == -1)
-			throw; // reuqire custom exception
-		fcntl(_server_fd[i].first, F_SETFL, O_NONBLOCK);
-	}
+  _event_list.resize(_server.size() * 8);
 }
 
 ws::Socket::~Socket() {}
 
-void ws::Socket::request_handler() {
-	
+void ws::Socket::kernel_handler() {
+  int new_events = 0;
+
+  while (1) {
+    _event_list.clear();
+    new_events = kevent(_kq, &_change_list[0], _change_list.size(), &_event_list[0], _event_list.size(), NULL);
+    if (new_events == -1)
+      throw; // require custom exception
+    _change_list.clear();
+
+    for (int i = 0; i < new_events; i++) {
+      struct kevent* curr_event = &_event_list[i];
+      server_type::iterator server_iter = _server.find(curr_event->ident);
+      client_type::iterator client_iter = _client.find(curr_event->ident);
+
+      if (curr_event->filter == EVFILT_READ) {
+        if (server_iter != _server.end()) {
+          int client_socket_fd;
+
+          if ((client_socket_fd = accept(curr_event->ident, NULL, NULL)) == -1)
+            throw; // require custom exception
+          fcntl(client_socket_fd, F_SETFL, O_NONBLOCK);
+          add_new_kevent(_change_list, client_socket_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+          _client.insert(client_type::value_type(client_socket_fd, &Socket::get_server_info));
+        }
+      }
+    }
+  }
+}
+
+/* Private function */
+
+void ws::Socket::add_new_kevent(kevent_vector& change_list, uintptr_t ident, int16_t filter,
+        uint16_t flags, uint32_t fflags, intptr_t data, void *udata)
+{
+    struct kevent temp_event;
+
+    EV_SET(&temp_event, ident, filter, flags, fflags, data, udata);
+    change_list.push_back(temp_event);
+}
+
+ws::Socket::server_type::iterator ws::Socket::get_server_info(int server_fd) {
+  return _server.find(server_fd);
 }
--- Socket/Socket.hpp
+++ Socket/Socket.hpp
@@ -14,28 +14,80 @@
 
 namespace ws {
 
-	class Socket {
-	
-	public:
-		typedef std::vector<std::pair<int, int> >	socket_type;
+  class Socket {
+  
+  public:
+    typedef std::map<int, struct sockaddr_in> server_type;
+    typedef server_type::iterator (Socket::*get_server_info_type) (int);
+    typedef std::map<int, get_server_info_type>   client_type;
+    typedef std::vector<struct kevent>        kevent_vector;
 
-	private:
-		std::vector<sockaddr_in> _server_addr;
+  private:
 
-		/*
-			first: server socket fd
-			second: server kqueue fd
-		*/
-		socket_type _server_fd;
+    /* ====================================== */
+    /*             Member Variable            */
+    /* ====================================== */
 
-		Socket& operator=(const Socket& cls);
-		Socket(const Socket& cls);
-		Socket();
+    /*
+      first: server socket fd
+      second: server socket address info
+    */
+    server_type _server;
 
-	public:
-		Socket(const Configure& cls);
-		~Socket();
+    /*
+      first: client socket fd
+      second: server socket fd
+    */
+    client_type _client;
 
-		void request_handler();
-	};
+    /*
+      새로 할당할 이벤트 목록을 갖고있는 벡터
+    */
+    kevent_vector _change_list;
+
+    /*
+      핸들링이 필요한 이벤트 목록을 담는 벡터
+    */
+    kevent_vector _event_list;
+
+    /*
+      kernel descriptor
+    */
+    int _kq;
+
+    /* ====================================== */
+    /*                  OCCF                  */
+    /* ====================================== */
+
+    Socket& operator=(const Socket& cls);
+    Socket(const Socket& cls);
+    Socket();
+
+    /* ====================================== */
+    /*            Private Function            */
+    /* ====================================== */
+
+    /*
+      insert new kevent to vector
+    */
+    void add_new_kevent(std::vector<struct kevent>& change_list, uintptr_t ident, int16_t filter,
+        uint16_t flags, uint32_t fflags, intptr_t data, void *udata);
+
+    server_type::iterator get_server_info(int fd);
+
+  public:
+
+    /* ====================================== */
+    /*                Structor                */
+    /* ====================================== */
+
+    Socket(const Configure& cls);
+    ~Socket();
+
+    /* ====================================== */
+    /*            Public Function             */
+    /* ====================================== */
+
+    void kernel_handler();
+  };
 }
