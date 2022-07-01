076f3418e449947072392e98c914395a69e228fa
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-07-01 18:33:36 +0900

    delete: useless function

--- Router/Kernel.hpp
+++ Router/Kernel.hpp
@@ -14,22 +14,8 @@
 namespace ws {
   class Kernel {
   
-  public:
-    typedef std::vector<struct kevent>        kevent_vector;
-  
   private:
-  
     int _kq;
-
-    /*
-      새로 할당할 이벤트 목록을 갖고있는 벡터
-    */
-    kevent_vector _change_list;
-
-    /*
-      핸들링이 필요한 이벤트 목록을 담는 벡터
-    */
-    kevent_vector _event_list;
 
     Kernel(const Kernel& cls);
     Kernel& operator=(const Kernel& cls);
@@ -38,20 +24,10 @@ namespace ws {
     Kernel();
     ~Kernel();
 
-    void  resize_event_list(int server_count);
-    void  reset_event_list();
-    void  reset_change_list();
-
     /*
       On call pending state until kernel fetches new events
     */
     void            kevent_ctl(uintptr_t ident, int16_t filter, uint16_t flags, uint32_t fflags, intptr_t data, void *udata);
     struct kevent   kevent_wait();
-
-
-    /*
-      Append data to look up to the kevent struct
-    */
-    struct kevent add_change_list(uintptr_t ident, int16_t filter, uint16_t flags, uint32_t fflags, intptr_t data, void *udata) throw();
   };
 }
