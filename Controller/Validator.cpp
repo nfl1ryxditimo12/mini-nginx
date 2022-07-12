108d312acca3dc8a444baf69671c948a16677ab0
Yeeun Ju <yeju@c1r3s3.42seoul.kr>
2022-07-12 22:37:12 +0900

    refactor: check_method error status code

--- Controller/Validator.cpp
+++ Controller/Validator.cpp
@@ -37,8 +37,7 @@ void ws::Validator::check_method(client_value_type& client_data) {
       if (*limit_except == "GET")
         return;
   }
-  client_data.status = FORBIDDEN;
-  // client_data.status = METHOD_NOT_ALLOWED; //todo
+  client_data.status = METHOD_NOT_ALLOWED;
 }
 
 void ws::Validator::check_uri(client_value_type& client_data) {
@@ -51,12 +50,12 @@ void ws::Validator::check_uri(client_value_type& client_data) {
     client_data.status = NOT_FOUND;
   } else {
     if (S_ISREG(file_status.st_mode)) {
-      // if (not html file) { //@
+      // if (not html file) {
       //   client_data.status = NOT_MODIFIED;
       //   return;
       // }
       client_data.status = OK;
-      if (file_status.st_mode & S_IREAD) //@읽기권한 없으면 403
+      if (file_status.st_mode & S_IREAD) //읽기권한 없으면 403
         client_data.status = FORBIDDEN;
     } else if (S_ISDIR(file_status.st_mode)) {
       if (url[url.length() - 1] == '/')
@@ -105,7 +104,9 @@ void ws::Validator::check_content_length(ws::Validator::client_value_type& clien
 void ws::Validator::check_connection(ws::Validator::client_value_type& client_data) {
   const std::string connection = client_data.request.get_connection();
 
-  if (!(connection == "close" || connection == "keep-alive" || "")) // todo
+  //close인데 close로 설정되어있지 않으면 error //todo
+
+  if (!(connection == "close" || connection == "keep-alive"))
     client_data.status = BAD_REQUEST;
 }
 
@@ -115,9 +116,10 @@ void ws::Validator::check_transfer_encoding(ws::Validator::client_value_type& cl
 
 void ws::Validator::check_host(ws::Validator::client_value_type& client_data) {
   std::string host = client_data.request.get_server_name();
+
   if (host == "")
     client_data.status = BAD_REQUEST;
-}
 
-// if (repository->empty())
-//   _status = ws::Code::NOT_FOUND;
+  //host가 2줄 이상 존재 -> 400
+  //host가 잘못된 값 -> 400
+}
