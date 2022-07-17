f787cae614a54c6cf393532bd83465499f0ea20f
jpham005 <jpham005@naver.com>
2022-07-17 23:04:17 +0900

    fix: wrong location parsing

--- Controller/Repository.cpp
+++ Controller/Repository.cpp
@@ -16,7 +16,7 @@
 
 ws::Repository::Repository(bool fatal, unsigned int status): _fatal(fatal), _status(status), _fd(FD_DEFAULT) {
   memset(&_file_stat, 0, sizeof(struct stat));
-  _index_root = ws::Util::get_root_dir();
+  _index_root = ws::Util::get_root_dir() + "/www";
 }
 
 ws::Repository::Repository(const Repository& cls): _fatal(cls._fatal), _status(cls._status) {
