163f705a21a0fb3f038b6663229eb6669e8e6a4d
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-07-18 15:39:33 +0900

    add: OCCF

--- Router/RequestParser.cpp
+++ Router/RequestParser.cpp
@@ -1,1 +1,67 @@
 #include "RequestParser.hpp"
+
+ws::RequestParser::RequestParser() {
+  _header_parser.insert(header_parse_map_type::value_type("Host", &Request::parse_host));
+  _header_parser.insert(header_parse_map_type::value_type("Connection", &Request::parse_connection));
+  _header_parser.insert(header_parse_map_type::value_type("Content-Length", &Request::parse_content_length));
+  _header_parser.insert(header_parse_map_type::value_type("Content-Type", &Request::parse_content_type));
+  _header_parser.insert(header_parse_map_type::value_type("Transfer-Encoding", &Request::parse_transfer_encoding));
+}
+
+ws::RequestParser::~RequestParser() {}
+
+void  ws::RequestParser::init_client_data(client_value_type &client_data) {
+  _fatal = &client_data.fatal;
+  _status = &client_data.status;
+  _request = &client_data.request;
+  _repository = &client_data.repository;
+}
+
+int   parse_request_message(const ws::Configure& conf, client_value_type& client_data, const char* message, const int read_size) {
+
+}
+
+void  ws::RequestParser::parse_host(const std::string& value) {
+  std::string::size_type pos = value.find_first_of(":");
+
+  if (!_server_name.empty()) {
+    _client_data.status = BAD_REQUEST;
+    return;
+  }
+
+  _server_name = value.substr(0, pos);
+  if (inet_addr(_server_name.c_str()) != INADDR_NONE) //ip
+    _server_name = "_";
+
+  if (pos != std::string::npos)
+    _port = ws::Util::stoul((value.substr(pos + 1, value.length())).c_str(), std::numeric_limits<u_int16_t>::max());
+}
+
+void  ws::RequestParser::parse_connection(const std::string& value) {
+  _connection = value;
+}
+
+void  ws::RequestParser::parse_content_length(const std::string& value) {
+  std::string::size_type content_length = ws::Util::stoul(value);
+
+  if (content_length == value.npos) {
+    _status = BAD_REQUEST;
+    return;
+  }
+
+  _content_length = content_length;
+}
+
+void  ws::RequestParser::parse_content_type(const std::string& value) {
+  //text/html; charset=utf-8
+  std::string::size_type pos = value.find(';');
+
+  _content_type = value.substr(0, pos);
+}
+
+void  ws::RequestParser::parse_transfer_encoding(const std::string& value) {
+  _transfer_encoding = value;
+  if (value == "chunked")
+    _chunked = true;
+  return;
+}
--- Router/RequestParser.hpp
+++ Router/RequestParser.hpp
@@ -5,14 +5,47 @@
 
 namespace ws {
   class RequestParser {
+
+  public:
+    typedef void (Request::*header_parse_func) (const std::string&);
+    typedef std::map<std::string, header_parse_func> header_parse_map_type;
+
+    typedef ws::Socket::client_value_type client_value_type;
+
   private:
+
+    header_parse_map_type _header_parser;
+
+    bool*         _fatal;
+    unsigned int* _status;
+    Request*      _request;
+    Repository*   _repository;
+
 
     RequestParser(const RequestParser& cls);
     RequestParser& operator=(const RequestParser& cls);
+
+    void  init_client_data(client_value_type& client_data);
+
+    void  parse_request_uri(std::string uri);
+    bool  parse_request_start_line();
+    void  parse_request_header();
+    void  parse_request_body();
+    void  parse_request_chunked_body();
+    void  parse_request_chunked_start_line();
+    void  parse_request_chunked_data_line();
+
+    void  parse_host(const std::string& value);
+    void  parse_connection(const std::string& value);
+    void  parse_content_length(const std::string& value);
+    void  parse_content_type(const std::string& value);
+    void  parse_transfer_encoding(const std::string& value);
+
+
   public:
     RequestParser();
     ~RequestParser();
 
-    int   parse_request_message(const ws::Configure& conf)
+    int   parse_request_message(const ws::Configure& conf, client_value_type& client_data, const char* message, const int read_size);
   };
 }
