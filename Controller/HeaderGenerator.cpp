502ca63813690f0c421b7d1c9608be4bc0266e9e
yeju <yeju@student.42seoul.kr>
2022-07-25 15:19:56 +0900

    fix: session header, type, add: curl.sh, error code 412

--- Controller/HeaderGenerator.cpp
+++ Controller/HeaderGenerator.cpp
@@ -128,7 +128,10 @@ void ws::HeaderGenerator::generate_connection_line(std::string& data) {
 }
 
 void ws::HeaderGenerator::generate_cookie(std::string& data, const client_value_type& client_data) {
-    data += "Set-Cookie: session_id=" + ws::Util::ultos(ws::Socket::get_session().find(client_data.request.get_session_id())->first) + "\r\n"; // todo
+//  unsigned int session_id = ws::Socket::get_session().find(client_data.request.get_session_id())->first;
+//    data += "Set-Cookie: session_id=" + ws::Util::ultos(session_id) + "\r\n"; // todo
 
-//    data += "Set-Cookie: session_id=" + ws::Util::ultos(client_data.request.get_session_id()) + "\r\n";
+  unsigned int session_id = client_data.request.get_session_id();
+  if (session_id != UINT_MAX)
+    data += "Set-Cookie: session_id=" + ws::Util::ultos(session_id) + "\r\n";
 }
--- curl.sh
+++ curl.sh
@@ -1,43 +1,36 @@
-# GET
-echo "\033[36m"
-echo "method: get"
-echo "url: http://localhost"
-echo "port: 9090"
-echo "\033[0m"
+# GET - root : OK
+echo "\033[36m GET - root : OK \033[0m"
 curl -X GET http://localhost:9090
 
-# 405, Method Not Allowed
-echo "\033[36m"
-echo "method: post"
-echo "url: http://localhost"
-echo "port: 9090"
-echo "\033[0m"
-curl -X POST http://localhost:9090
-
-# POST
-echo "\033[36m"
-echo "method: post"
-echo "url: http://localhost/post_body"
-echo "port: 9090"
-echo "body: 01234567890"
-echo "\033[0m"
+# POST - post_body : OK
+echo "\033[36m POST - post_body : OK \033[0m"
 curl -X POST http://localhost:9090/post_body -d "1234567890"
 
-# 413, Payload Too Large
-#echo "\033[36m"
-#echo "method: post"
-#echo "url: http://localhost/post_body"
-#echo "port: 9090"
-#echo "body: 1234567890"
-#echo "\033[0m"
-#curl -X POST http://localhost:9090/post_body -d "1234567890 ..."
+# POST/PUT - root : 405, Method Not Allowed
+echo "\033[36m POST - root : 405, Method Not Allowed \033[0m"
+curl -X POST http://localhost:9090
+
+# POST/PUT - post_body : 413, Payload Too Large
+echo "\033[36m POST - post_body : 413, Payload Too Large \033[0m"
+curl -X POST http://localhost:9090/post_body -d "12345678901"
 
 # HEAD
-curl -I HEAD http://localhost:9090/
+# curl -I HEAD http://localhost:9090/
+
+# DELETE
+echo "\033[36m DELETE - root : 405, Method Not Allowed \033[0m"
+curl -X DELETE http://localhost:9090/
+
+# PUT
+curl -X PUT http://localhost:9090/
 
 #header
 curl -X POST http://localhost:9090/post_body -H "Content-Length=5&Content-Type=text/html"
 
+# GET, session
+curl -X GET http://localhost:9090/session -H "Secret-Key=hellowebserv&Cookie=session_id=2&Name=yeju"
+# POST, session
+# DELETE, session
 
 
 
